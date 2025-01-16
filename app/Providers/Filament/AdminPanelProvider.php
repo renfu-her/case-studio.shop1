eb7ebce5bfdc9d53bad5109906ae2e34eaa925c4
Patrick Her <renfu.her@gmail.com>
2025-01-16 15:02:08 +0800

    feat(admin panel): add products page and set default home URL for admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -40,11 +40,10 @@ class AdminPanelProvider extends PanelProvider
                 'success' => Color::Emerald,
                 'warning' => Color::Orange,
             ])
+            ->homeUrl('/admin/products')
+            ->default()
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
-            ->pages([
-                Pages\Dashboard::class,
-            ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
---
+++ resources/views/filament/pages/products.blade.php
@@ -0,0 +1,3 @@
+<x-filament-panels::page>
+    {{-- 這個頁面不需要內容，因為會立即重定向 --}}
+</x-filament-panels::page>

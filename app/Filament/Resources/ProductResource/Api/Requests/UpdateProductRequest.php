a46bacc4a5899e8c18f7147f386cf8cf7d17e5d4
Patrick Her <renfu.her@gmail.com>
2025-01-22 22:20:23 +0800

    feat(api-service): implement Product API service with CRUD handlers and validation requests; add configuration for API service integration and update Filament admin panel to include the new service

---
+++ app/Filament/Resources/ProductResource/Api/Requests/UpdateProductRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\ProductResource\Api\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+			'category_id' => 'required|integer',
+			'name' => 'required|string',
+			'description' => 'required|string',
+			'image' => 'required|string',
+			'price' => 'required|numeric',
+			'stock' => 'required|integer',
+			'is_active' => 'required|integer',
+			'is_hot' => 'required|integer',
+			'is_new' => 'required|integer'
+		];
+    }
+}
--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -17,6 +17,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Rupadana\ApiService\ApiServicePlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -68,6 +69,8 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])->plugins([
+                ApiServicePlugin::make()
             ]);
     }
 }

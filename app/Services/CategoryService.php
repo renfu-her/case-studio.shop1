8fc7c8c5a7ba64ee23442585124eefd31a1041ad
Patrick Her <renfu.her@gmail.com>
2025-04-12 20:41:05 +0800

    fix(分類服務): 移除分類圖片的回退URL設定，以簡化圖片欄位的邏輯

--- app/Services/CategoryService.php
+++ app/Services/CategoryService.php
@@ -108,8 +108,7 @@ class CategoryService extends BaseService
     private function getImageColumn()
     {
         return $this->createImageColumn('image', '分類圖片')
-            ->defaultImageUrl(asset('assets/images/no-image.svg'))
-            ->fallbackImageUrl(asset('assets/images/no-image.svg'));
+            ->defaultImageUrl(asset('assets/images/no-image.svg'));
     }
 
     private function getNameColumn()

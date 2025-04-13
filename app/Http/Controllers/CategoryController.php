f1856d5ddc003e2c7818d91a054c12b5d1cb0536
Patrick Her <renfu.her@gmail.com>
2025-04-13 17:53:03 +0800

    調試CategoryController中的show方法，新增dd()以顯示根分類及當前分類的完整路徑，便於開發過程中的問題排查。

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -45,6 +45,8 @@ class CategoryController extends Controller
         
         // 使用 Service 獲取當前分類的完整路徑
         $categoryPath = $this->categoryService->getCategoryPath($category);
+
+        dd($rootCategories, $categoryPath);
         
         return view('categories.show', compact('category', 'subcategories', 'products', 'rootCategories', 'categoryPath'));
     }
